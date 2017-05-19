a91060316125479add96d457faf6d2f4f2f4b8aa
Justin VanWinkle <vanwinkle.justin@gmail.com>
2017-05-19 09:34:30 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Circa 2012 - The first website that I built and maintained regularly as a professional.
+
+The site can be seen at [justin-vanwinkle.github.io/erinWebsite](http://justin-vanwinkle.github.io/erinWebsite) 
---
+++ javascripts/scale.fix.js
@@ -0,0 +1,23 @@
+var metas = document.getElementsByTagName('meta');
+var i;
+window.onload = function() {
+	if(!window.location.hash) {
+		window.location = window.location + '#refreshed';
+		window.location.reload();
+	}
+}
+if (navigator.userAgent.match(/iPhone/i)) {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+    }
+  }
+  document.addEventListener("gesturestart", gestureStart, false);
+}
+function gestureStart() {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    }
+  }
+}
